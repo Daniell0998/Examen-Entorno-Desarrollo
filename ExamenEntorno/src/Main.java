74b3181f5250352108b8d1320f61043dced8a26a
usuario <usuario@DESKTOP-0MUMB30>
2022-12-07 20:12:09 +0100

    Nuevo Syso
    Hasta luego.

--- ExamenEntorno/src/Main.java
+++ ExamenEntorno/src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("Hola me llamo");
-
+		System.out.println("Hola me llamo");
+		System.out.println("Hasta Luego");
 	}
 
 }
